ca0dcd90fc745b30d1ef7c2e01ad2fd90dfa7c7e
Ilya Zakharov <74999345+Suppolyar@users.noreply.github.com>
2023-08-05 17:03:38 +0400

    feat: Component Card of book added

--- src/components/card-list/card-list.js
+++ src/components/card-list/card-list.js
@@ -1,5 +1,6 @@
 import { DivComponent } from "../../common/div-component.js";
 import './card-list.css'
+import {Card} from "../card/card.js";
 export class CardList extends DivComponent {
     constructor(appState, parentState) {
         super()
@@ -17,8 +18,11 @@ export class CardList extends DivComponent {
         this.el.classList.add('card-list')
 
         this.el.innerHTML = `
-            <h1>Найдено книг - ${this.parentState.list.length}</h1>
+            <h1>Найдено книг - ${this.parentState.numFound}</h1>
         `
+        for (const card of this.parentState.list) {
+            this.el.append(new Card(this.appState, card).render())
+        }
 
         return this.el
     }
